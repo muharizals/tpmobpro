4359c93097427616ef1f76343c909164af073f92
muharizals <muhammadarizals1@gmail.com>
2016-09-05 06:08:42 +0700

    TP modul 1,2 Mobile Programming

--- src/main/java/com/tumpi/tepesatu/profile/ProfileActivity.java
+++ src/main/java/com/tumpi/tepesatu/profile/ProfileActivity.java
@@ -1,18 +1,53 @@
 package com.tumpi.tepesatu.profile;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.tumpi.tepesatu.R;
 
-public class ProfileActivity extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
 
+public class ProfileActivity extends AppCompatActivity {
+    private CircleImageView gambar;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        gambar =(CircleImageView)findViewById(R.id.gambar);
+    }
+
+    private boolean isSupportCamera(){
+        return getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) ? true : false;
+    }
+
+    public void takePicture(View view){
+        if(isSupportCamera()){
+            Intent take = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if(take.resolveActivity(getPackageManager()) != null){
+                startActivityForResult(take,REQUEST_IMAGE_CAPTURE);
+            }
+        }
+        else{
+            Toast.makeText(this,"camera not support",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extra = data.getExtras();
+            Bitmap btm = (Bitmap)extra.get("data");
+            gambar.setImageBitmap(btm);
+        }
     }
 
     @Override
